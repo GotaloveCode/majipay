14b99ab2ee7a87d78f38b09a212b80c93e211aa5
Martin Kagume <martiniriga@gmail.com>
2017-07-22 17:28:31 +0300

    initial commit

---
+++ Common/INavigable.cs
@@ -0,0 +1,9 @@
+﻿namespace MajiPay
+{
+    public interface INavigable
+    {
+        void Activate(object parameter);
+        void Deactivate(object parameter);
+        //bool AllowGoBack();
+    }
+}
